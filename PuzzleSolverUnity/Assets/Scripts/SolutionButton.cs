226621f92d00ef39477db20b5612afd992d6dcc5
Nick Lemmon <lemm0016@algonquinlive.com>
2021-03-28 15:30:18 -0400

    Created a dynamic set of buttons for the Unity project.  This will allow me to later display the correct solution inside of unity

---
+++ PuzzleSolverUnity/Assets/Scripts/SolutionButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SolutionButton : MonoBehaviour
+{
+    public int SolutionIndex { get; set; }
+
+    public Solver solver;
+
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        if(button != null)
+        {
+            button.onClick.AddListener(OnButtonClick);
+
+            Text t = button.GetComponentInChildren<Text>();
+            if(t != null)
+            {
+                t.text = (SolutionIndex + 1).ToString();
+            }
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        solver.DisplaySolution(SolutionIndex);
+    }
+
+}
--- PuzzleSolverUnity/Assets/Scripts/Solver.cs
+++ PuzzleSolverUnity/Assets/Scripts/Solver.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Solver : MonoBehaviour
 {
+
+    public GameObject ButtonPrefab;
+    public GameObject canvas;
+
+    public int Solutions = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,38 @@ public class Solver : MonoBehaviour
         int Solutions = m_PuzzleSolver.GetNumSolutions();
 
         Debug.Log("Solutions: " + Solutions.ToString());
+
+
+        int RowsCount = (Solutions - 1) / 10;
+
+        for(int i = 0; i < Solutions; i++)
+        {
+            GameObject newButton = Instantiate<GameObject>(ButtonPrefab);
+
+            //Set position before placing inside the Canvas
+            int InitialXPos = 400 - (RowsCount * 45);
+            int InitialYPos = 200;
+
+            int XOffset = 45 * (i / 10);
+            int YOffset = 45 * (i % 10);
+
+            int XPos = InitialXPos + XOffset;
+            int YPos = InitialYPos - YOffset;
+
+            newButton.transform.position = new Vector3(XPos, YPos);
+            newButton.transform.SetParent(canvas.transform, false);
+
+            //Setup the variables
+            SolutionButton solutionButton = newButton.GetComponent<SolutionButton>();
+            solutionButton.SolutionIndex = i;
+            solutionButton.solver = this;
+        }
+    }
+
+
+    public void DisplaySolution(int solutionNum)
+    {
+        Debug.Log("Button pressed: " + solutionNum.ToString());
     }
 
 

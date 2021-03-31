e957577790300ec931455824046014d62a3be577
Nick Lemmon <lemm0016@algonquinlive.com>
2021-03-31 00:11:23 -0400

    Designed a little nicer Piece Editor layout

--- PuzzleSolverUnity/Assets/Editor/PuzzleDesignerEditor.cs
+++ PuzzleSolverUnity/Assets/Editor/PuzzleDesignerEditor.cs
@@ -7,48 +7,52 @@ using UnityEngine;
 [CustomEditor(typeof(PuzzleLayout))]
 public class PuzzleDesignerEditor : Editor
 {
-    SerializedProperty[] Properties = new SerializedProperty[64];
-
+    SerializedProperty PuzzlePieceData;
     public void OnEnable()
     {
-        
+        PuzzlePieceData = serializedObject.FindProperty("LayoutData");
     }
 
-    //public override void OnInspectorGUI()
-    //{
-    //    serializedObject.Update();
-    //
-    //    PuzzleLayout pLayout = (PuzzleLayout)target;
-    //
-    //    if(pLayout != null)
-    //    {
-    //        for(int i = 0; i < 64; i++)
-    //        {
-    //             string PieceLabel = "Piece ";
-    //             PieceLabel += (i / 4).ToString();
-    //             PieceLabel += " ";
-    //
-    //             switch (i % 4)
-    //             {
-    //                 case 0:
-    //                     PieceLabel += "top:";
-    //                     break;
-    //                 case 1:
-    //                     PieceLabel += "right: ";
-    //                     break;
-    //                 case 2:
-    //                     PieceLabel += "bottom: ";
-    //                     break;
-    //                 case 3:
-    //                     PieceLabel += "left: ";
-    //                     break;
-    //             }
-    //
-    //             GUILayout.BeginHorizontal();
-    //             EditorGUILayout.LabelField(PieceLabel);
-    //             EditorGUILayout.IntField(pLayout.LayoutData[i]);
-    //             GUILayout.EndHorizontal();
-    //        }
-    //    }
-    //}
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+    
+        PuzzleLayout pLayout = (PuzzleLayout)target;
+    
+        if(pLayout != null)
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                string PieceLabel = "Piece ";
+                PieceLabel += (i + 1).ToString();
+
+                EditorGUILayout.LabelField(PieceLabel);
+
+                EditorGUI.indentLevel++;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    string SideLabel = "Side: ";
+                    switch (j)
+                    {
+                        case 0:
+                            SideLabel += "top:";
+                            break;
+                        case 1:
+                            SideLabel += "right: ";
+                            break;
+                        case 2:
+                            SideLabel += "bottom: ";
+                            break;
+                        case 3:
+                            SideLabel += "left: ";
+                            break;
+                    }
+                    EditorGUILayout.LabelField(SideLabel);
+                    pLayout.LayoutData[i*4+j] = EditorGUILayout.IntField(pLayout.LayoutData[i*4+j]);
+                }
+                EditorGUI.indentLevel--;
+            }
+        }
+    }
 }

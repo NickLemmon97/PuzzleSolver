4e903f305ca61b99045863d7a36942791e759e59
Nick Lemmon <lemm0016@algonquinlive.com>
2021-03-31 22:55:23 -0400

    Implemented a basic Editor Window that allows for opening a puzzle layout.  It displays the int value of the side at the side and rough location of the side on the piece

--- PuzzleSolverUnity/Assets/Editor/PuzzleDesignerWindow.cs
+++ PuzzleSolverUnity/Assets/Editor/PuzzleDesignerWindow.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class PuzzleDesignerWindow : EditorWindow
 {
+
+    public PuzzleLayout CurrentLayout;
+
     [MenuItem("Puzzle/Puzzle Designer")]
     static void Init()
     {
@@ -12,8 +15,133 @@ public class PuzzleDesignerWindow : EditorWindow
         window.Show();
     }
 
+    private void OnEnable()
+    {
+        if (EditorPrefs.HasKey("ObjectPath"))
+        {
+            string objectPath = EditorPrefs.GetString("ObjectPath");
+            CurrentLayout = AssetDatabase.LoadAssetAtPath(objectPath, typeof(PuzzleLayout)) as PuzzleLayout;
+        }
+    }
+
     private void OnGUI()
     {
-        GUILayout.Label("Demo");
+        DisplayHeader();
+
+        if (CurrentLayout == null)
+        {
+            return;
+        }
+
+        DisplayLayout();
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(CurrentLayout);
+        }
     }
+
+    void DisplayHeader()
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Open Puzzle Layout", GUILayout.ExpandWidth(false)))
+            {
+                OpenPuzzleLayout();
+            }
+
+            if (GUILayout.Button("New Puzzle Layout", GUILayout.ExpandWidth(false)))
+            {
+                CreatePuzzleLayout();
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void DisplayLayout()
+    {
+        GUILayout.Label("Layout:");
+
+        for (int pieces = 0; pieces < 64; pieces += 16)
+        {
+            //Top Pieces
+            EditorGUILayout.BeginHorizontal();
+            {
+                CreatePieceLocationPrespacing(pieces, true);
+                CreatePieceLocationPrespacing(pieces + 4, true);
+                CreatePieceLocationPrespacing(pieces + 8, true);
+                CreatePieceLocationPrespacing(pieces + 12, true);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //Left-Right
+            EditorGUILayout.BeginHorizontal();
+            {
+                CreatePieceLocation(pieces + 3);
+                CreatePieceLocation(pieces + 1, true);
+
+                CreatePieceLocation(pieces + 7);
+                CreatePieceLocation(pieces + 5, true);
+
+                CreatePieceLocation(pieces + 11);
+                CreatePieceLocation(pieces + 9, true);
+
+                CreatePieceLocation(pieces + 15);
+                CreatePieceLocation(pieces + 13, true);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //Bottom pieces
+            EditorGUILayout.BeginHorizontal();
+            {
+                CreatePieceLocationPrespacing(pieces + 2, true);
+                CreatePieceLocationPrespacing(pieces + 6, true);
+                CreatePieceLocationPrespacing(pieces + 10, true);
+                CreatePieceLocationPrespacing(pieces + 14, true);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //Ending space
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Space();
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    void OpenPuzzleLayout()
+    {
+        string absPath = EditorUtility.OpenFilePanel("Select Puzzle Layout", "", "");
+        if (absPath.StartsWith(Application.dataPath))
+        {
+            string relPath = absPath.Substring(Application.dataPath.Length - "Assets".Length);
+            CurrentLayout = AssetDatabase.LoadAssetAtPath(relPath, typeof(PuzzleLayout)) as PuzzleLayout;
+            if (CurrentLayout)
+            {
+                EditorPrefs.SetString("ObjectPath", relPath);
+            }
+        }
+    }
+
+    void CreatePuzzleLayout()
+    {
+        //TODO: Implement
+    }
+
+    void CreatePieceLocation(int index, bool withspace = false, int spacing = 1)
+    {
+        CurrentLayout.LayoutData[index] = EditorGUILayout.IntField(CurrentLayout.LayoutData[index]);
+
+        if (withspace)
+        {
+            EditorGUILayout.Space(spacing);
+        }
+    }
+
+    void CreatePieceLocationPrespacing(int index, bool withspace = false, int postspace = 1, int prespace = 10)
+    {
+        EditorGUILayout.Space(prespace);
+
+        CreatePieceLocation(index, withspace, postspace);
+    }
+
 }
